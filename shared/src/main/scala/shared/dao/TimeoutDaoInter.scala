b604b7c9f78a9e66f1ce4efbb659e374d0a7fa6f
Greg Dorrell <greg@dorrell.me>
2015-12-03 00:54:45 +0000

    add missing file

---
+++ shared/src/main/scala/shared/dao/TimeoutDaoInter.scala
@@ -0,0 +1,17 @@
+package shared.dao
+
+import shared.model.{Entry, LatLong}
+
+/**
+  * Created by Greg Dorrell on 02/12/2015.
+  */
+trait TimeoutDaoInter {
+
+  def getGeocode(locationId: String): Option[LatLong]
+
+  def getAddress(locationId: String): Option[String]
+
+  def getPage(articleId: String): String
+
+  def getEntries(articleId: String): List[Entry]
+}
