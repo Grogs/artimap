5a3c7f5f95327864376a3cc106239f1b96ae927e
Greg Dorrell <Greg@Dorrell.me>
2015-12-20 19:29:38 +0000

    Sometimes articles link to dead pages (on their own site!). Remove those dead entries in MapService.

--- server/src/main/scala/server/service/MapService.scala
+++ server/src/main/scala/server/service/MapService.scala
@@ -1,5 +1,8 @@
 package server.service
 
+import java.io.FileNotFoundException
+
+import com.typesafe.scalalogging.LazyLogging
 import shared.dao.{TimeoutDaoInter, GeocodingDaoInter}
 import shared.model.{LatLong, Entry}
 import shared.service.MapServiceInter
@@ -8,26 +11,32 @@ import scala.concurrent.Future
 import server.util.MutableMapPimp
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class MapService(geocodingDao: GeocodingDaoInter, timeoutDao: TimeoutDaoInter, cache: mutable.Map[String, List[(Entry, LatLong)]]) extends MapServiceInter {
+class MapService(geocodingDao: GeocodingDaoInter, timeoutDao: TimeoutDaoInter, cache: mutable.Map[String, List[(Entry, LatLong)]]) extends MapServiceInter with LazyLogging {
 
   private def getPosition(entry: Entry): Future[LatLong] = {
-    timeoutDao.getGeocode(entry.relPath) match {
+    Future(timeoutDao.getGeocode(entry.relPath)).flatMap(_ match {
       case Some(validPos) => Future.successful(validPos)
       case None =>
         val address = timeoutDao.getAddress(entry.relPath)
         geocodingDao.retrieveLatLong(address getOrElse entry.address)
-    }
+    })
   }
+
+  def remove404s(positions: List[Future[(Entry, LatLong)]]) = positions.map(_.map(Option(_)).recover{
+    case error404: FileNotFoundException =>
+      logger.error("received 404 when retrieving position, skipping", error404)
+      None
+  })
 
   def getPositionedEntries(article: String): Future[List[(Entry, LatLong)]] = {
     cache.getOrElseUpdateAsync(article, {
       val entries = timeoutDao.getEntries(article)
-      val res = Future.sequence(for {
+      val res = Future.sequence(remove404s(for {
         entry <- entries
       } yield for {
         pos <- getPosition(entry)
         res = entry -> pos
-      } yield res)
+      } yield res)).map(_.flatten)
       res
     })
 
